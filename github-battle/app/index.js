86954047bb399f49817f61d6b53be83dd8d7f390
christopher-lamkin <cjlamkin64@gmail.com>
2017-03-30 08:55:56 -0700

    create avatar component with children components for profile name, image, and link

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,12 +1,109 @@
+var USER_DATA = {
+  name: 'Chris Lamkin',
+  username: 'christopher-lamkin',
+  imageUrl: 'https://www.sonicstadium.org/wp-content/uploads/2009/02/us-art1.png'
+};
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
-  render: function () {
+var ProfilePic = React.createClass({
+  render: function(){
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function(){
     return (
-      <div>Hello ReactJS Program!</div>
+      <div>
+        <a href={"https://github.com/" + this.props.username}>{this.props.username}</a>
+      </div>
     )
   }
 });
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+var ProfileName = React.createClass({
+  render: function(){
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render:function(){
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.imageUrl} />
+        <ProfileLink username={this.props.user.username} />
+        <ProfileName name={this.props.user.name} />
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <Avatar user={USER_DATA} />,
+  document.getElementById('app')
+);
+
+
+
+
+
+
+
+// var USER_DATA = {
+//   name: 'Chris Lamkin',
+//   username: 'christopher-lamkin',
+//   image: 'https://www.sonicstadium.org/wp-content/uploads/2009/02/us-art1.png'
+// };
+//
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+//
+// var ProfilePic = React.createClass({
+//   render: function(){
+//     return <img src={this.props.image} style={{height: 100, width: 100}}></img>
+//   }
+// });
+//
+// var ProfileLink = React.createClass({
+//   render: function(){
+//     return (
+//     <div>
+//       <a href={'https://github.com/' + this.props.username}>{this.props.username}</a>
+//     </div>
+//     )
+//   }
+// });
+//
+// var ProfileName = React.createClass({
+//   render: function(){
+//     return (
+//       <div>
+//         {this.props.name}
+//       </div>
+//     )
+//   }
+// });
+//
+// var Avatar = React.createClass({
+//   render: function(){
+//     return (
+//       <div>
+//         <ProfilePic image={this.props.user.image} />
+//         <ProfileName name={this.props.user.name} />
+//         <ProfileLink username={this.props.user.username} />
+//       </div>
+//     )
+//   }
+// })
+//
+// ReactDOM.render(
+//   <Avatar user={USER_DATA}/>,
+//   document.getElementById('app')
+// );
